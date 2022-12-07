ea73d27d2f352fb4d2672b46dca4350dcae2f3c2
Stas Dashkevich <>
2022-12-07 16:10:06 +0300

    init

---
+++ dogTranslator/dogTranslator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dogTranslator
+//
+//  Created by Stas Dashkevich on 4.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
